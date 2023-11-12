7131a520578fb4bbc5514162231fe375f2fa01eb
Wiktoria <wiktorialopatka222@gmail.com>
2023-11-12 17:29:23 +0100

    lab-c

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MeasurementRepository;
+use App\Repository\LocationRepository;
+
+
+
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/{id}', name: 'app_weather', requirements: ['id' => '\d+'])]
+public function city(Location $location, MeasurementRepository $repository): Response
+{
+    $measurements = $repository->findByLocation($location);
+    
+    return $this->render('weather/city.html.twig', [
+        'location' => $location,
+        'measurements' => $measurements,
+    ]);
+}
+
+}
